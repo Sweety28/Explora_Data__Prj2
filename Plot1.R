b51a98020d7ae394267198af6c3ec991ba23ee04
Sweety28 <33377171+Sweety28@users.noreply.github.com>
2017-11-04 17:52:47 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -28,3 +28,19 @@ download.file(get.data.project,destfile="./dataStore/exdata-data-NEI_data.zip",m
 path_rf <- file.path("./dataStore" , "exdata-data-NEI_data")
 files<-list.files(path_rf, recursive=TRUE)
 files
+
+NEI <- readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
+number.add.width<-800                             # width length to make the changes faster
+number.add.height<-800                             # height length to make the changes faster
+require(dplyr)
+
+total.emissions <- summarise(group_by(NEI, year), Emissions=sum(Emissions))
+clrs <- c("red", "green", "blue", "yellow")
+x1<-barplot(height=total.emissions$Emissions/1000, names.arg=total.emissions$year,
+        xlab="years", ylab=expression('total PM'[2.5]*' emission in kilotons'),ylim=c(0,8000),
+        main=expression('Total PM'[2.5]*' emissions at various years in kilotons'),col=clrs)
+
+## Add text at top of bars
+text(x = x1, y = round(total.emissions$Emissions/1000,2), label = round(total.emissions$Emissions/1000,2), pos = 3, cex = 0.8, col = "black")
+
